360b728540f64ba8238b098258b248f0a9375b80
Nunes, Alexandre T <alexandre.t.nunes@accenture.com>
2020-01-21 20:12:29 -0300

    Criacao do Greeting

---
+++ src/main/test/Greeter.java
@@ -0,0 +1,12 @@
+package main.test;
+
+public class Greeter {
+
+	public String sayGreetings() {
+		return "Hello World!";
+	}
+	
+	public String yellGreetings() {
+		return "HELLO WORLD!!!";
+	}
+}
---
+++ src/main/test/Main.java
@@ -0,0 +1,12 @@
+package main.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		
+		System.out.println(greeter.sayGreetings());
+
+	}
+
+}
